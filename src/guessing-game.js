2d9820f0d3c6310d6dffb1f4f0625a7a1b258d54
EugeneShatalin <shatalin_ei@mail.ru>
2021-09-05 20:53:14 +0600

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.resultNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.resultNumber = Math.round((this.min + this.max) / 2);
+        return this.resultNumber;
     }
 
     lower() {
-
+        this.max = this.resultNumber;
     }
 
     greater() {
-
+        this.min = this.resultNumber;
     }
 }
 
